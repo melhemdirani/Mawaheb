059f0770f09bdd6fd09a80e43c1930a9944f5573
melhemdirani <melhemdirani@hotmail.com>
2022-07-27 16:14:53 +0300

    fixing issues after merge

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LandingPage from './pages/LandingPage/LandingPage';
-import SignupPage from './pages/SignupPage/SignupPage';
+import LandingPage from './pages/LandingPage';
+import SignupPage from './pages/SignupPage';
 import TestingComponentPage from './pages/TestingComponentPage';
 
 const Stack = createNativeStackNavigator();
