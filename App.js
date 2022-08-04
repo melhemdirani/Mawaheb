c07e9689ea935a7d4151799ba9b56b3531ab6e1f
Mirza-LB <mirzadev.lb@gmail.com>
2022-08-04 13:29:33 +0300

    redux toolkit

--- App.js
+++ App.js
@@ -1,42 +1,41 @@
-import React, { useMemo } from 'react';
-import { ActivityIndicator } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { store , persistor} from './redux/store';
-import { Provider } from 'react-redux';
-import { NotifierWrapper } from 'react-native-notifier';
-import { PersistGate } from 'redux-persist/integration/react';
+import React, { useMemo } from 'react'
+import { ActivityIndicator } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { store, persistor } from './redux/store'
+import { Provider } from 'react-redux'
+import { NotifierWrapper } from 'react-native-notifier'
+import { PersistGate } from 'redux-persist/integration/react'
+import { toolkitStore } from './reduxToolkit/store'
 
-import LandingPage from './pages/LandingPage';
-import SignupPage from './pages/SignupPage';
-import SettingsPage from './pages/SettingsPage';
-import { useFonts } from 'expo-font';
-import CreateProfilePage from './pages/CreateProfilePage';
-import ExperiencePage from './pages/ExperiencePage';
-import LanguagePage from './pages/LanguagePage';
-import BankPage from './pages/BankPage';
-import ClientSignupPage from './pages/ClientSignupPage';
-import Navbar from './components/Navbar';
-import JobsPage from './pages/JobsPage';
-import JobListPage from './pages/JobListPage';
-import JobseekerDashboard from './pages/JobseekerDashboard';
-import NotificationsPage from './pages/NotificationsPage';
-import FreelanceAcceptedPage from './pages/FreelanceAcceptedPage';
-import FreelancerDetailsPage from './pages/FreelancerDetailsPage';
-import JobDonePage from './pages/JobDonePage';
-import JobPostingPage from './pages/JobPostingPage';
-import PaymentPage from './pages/PaymentPage';
-import LoginJobseeker from './pages/LoginJobseeker';
-import JobContractPage from './pages/JobContractPage';
-import JobSeekersignup from './pages/JobSeekersignup';
-import ClientDashboard from './pages/ClientDashboard';
+import LandingPage from './pages/LandingPage'
+import SignupPage from './pages/SignupPage'
+import SettingsPage from './pages/SettingsPage'
+import { useFonts } from 'expo-font'
+import CreateProfilePage from './pages/CreateProfilePage'
+import ExperiencePage from './pages/ExperiencePage'
+import LanguagePage from './pages/LanguagePage'
+import BankPage from './pages/BankPage'
+import ClientSignupPage from './pages/ClientSignupPage'
+import Navbar from './components/Navbar'
+import JobsPage from './pages/JobsPage'
+import JobListPage from './pages/JobListPage'
+import JobseekerDashboard from './pages/JobseekerDashboard'
+import NotificationsPage from './pages/NotificationsPage'
+import FreelanceAcceptedPage from './pages/FreelanceAcceptedPage'
+import FreelancerDetailsPage from './pages/FreelancerDetailsPage'
+import JobDonePage from './pages/JobDonePage'
+import JobPostingPage from './pages/JobPostingPage'
+import PaymentPage from './pages/PaymentPage'
+import LoginJobseeker from './pages/LoginJobseeker'
+import JobContractPage from './pages/JobContractPage'
+import JobSeekersignup from './pages/JobSeekersignup'
+import ClientDashboard from './pages/ClientDashboard'
 
-import JobDetailsPage from './pages/JobDetailsPage';
-import JobSeekersignup2 from './pages/JobSeekersignup2';
-import * as Sentry from "@sentry/react-native";
-
-
+import JobDetailsPage from './pages/JobDetailsPage'
+import JobSeekersignup2 from './pages/JobSeekersignup2'
+import * as Sentry from '@sentry/react-native'
 
 const Stack = createNativeStackNavigator()
 
@@ -53,178 +52,174 @@ function App() {
   }
 
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-
-        <NavigationContainer>
-            <Stack.Navigator>
-              <Stack.Screen
-                name="Home"
-                component={LandingPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="SignIn"
-                component={SignupPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="JobSignUp2"
-                component={CreateProfilePage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="JobSignUp"
-                component={JobSeekersignup}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="JobSignUpb"
-                component={JobSeekersignup2}
-                options={{
-                  headerShown: false,
-              }}
-              />
-              <Stack.Screen
-                name="experience"
-                component={ExperiencePage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="language"
-                component={LanguagePage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="bank"
-                component={BankPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="jobseeker_jobs"
-                component={JobsPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="login"
-                component={LoginJobseeker}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="seeker_dash"
-                component={JobseekerDashboard}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="notifications"
-                component={NotificationsPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="settings"
-                component={SettingsPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="recruiter_signup"
-                component={ClientSignupPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="recruiter_dashboard"
-                component={ClientDashboard}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="recruiter_Jobs"
-                component={JobListPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="jobPosting"
-                component={JobPostingPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="payment"
-                component={PaymentPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="freelancerDetails"
-                component={FreelancerDetailsPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="acceptContract"
-                component={JobContractPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="acceptedClient"
-                component={FreelanceAcceptedPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="jobDoneClient"
-                component={JobDonePage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="jobDescription"
-                component={JobDetailsPage}
-                options={{
-                  headerShown: false,
-                }}
-              />
-            </Stack.Navigator>
-        </NavigationContainer>
-        </PersistGate>
+    <Provider store={toolkitStore}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Home'
+            component={LandingPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='SignIn'
+            component={SignupPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='JobSignUp2'
+            component={CreateProfilePage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='JobSignUp'
+            component={JobSeekersignup}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='JobSignUpb'
+            component={JobSeekersignup2}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='experience'
+            component={ExperiencePage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='language'
+            component={LanguagePage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='bank'
+            component={BankPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='jobseeker_jobs'
+            component={JobsPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='login'
+            component={LoginJobseeker}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='seeker_dash'
+            component={JobseekerDashboard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='notifications'
+            component={NotificationsPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='settings'
+            component={SettingsPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='recruiter_signup'
+            component={ClientSignupPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='recruiter_dashboard'
+            component={ClientDashboard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='recruiter_Jobs'
+            component={JobListPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='jobPosting'
+            component={JobPostingPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='payment'
+            component={PaymentPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='freelancerDetails'
+            component={FreelancerDetailsPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='acceptContract'
+            component={JobContractPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='acceptedClient'
+            component={FreelanceAcceptedPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='jobDoneClient'
+            component={JobDonePage}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='jobDescription'
+            component={JobDetailsPage}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
-
   )
 }
 
-export default App;
+export default App
---
+++ reduxToolkit/userSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+import customFetch from '../utils/axios'
+
+const initialState = {
+  user: {},
+  isLoading: false,
+  error: null,
+}
+
+export const registerUser = createAsyncThunk(
+  'registerUser',
+  async (user, thunkApi) => {
+    let url = '/auth/register'
+    try {
+      const resp = await customFetch.post(url, user)
+
+      return resp.data
+    } catch (error) {
+      console.log(error.response.data.msg)
+      return thunkApi.rejectWithValue(error.response.data.msg)
+    }
+  }
+)
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    toggleSidebar: (state,action)=>{
+      state.isSidebarOpen = action.payload
+    }
+    
+  },
+  extraReducers: {
+    [registerUser.pending]: (state) => {
+      state.isLoading = true
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload
+      state.isLoading = false
+      state.user = user
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      state.error = payload
+    },
+  },
+})
+export const {toggleSidebar}=userSlice.actions
+
+export default userSlice.reducer
